9a143943b62b62f8e1db710b8f913b02676283d2
Shirasawa-CN <3489840593@qq.com>
2024-04-26 23:51:32 +0800

    cargo fmt

--- src/dao/mod.rs
+++ src/dao/mod.rs
@@ -1,26 +1,24 @@
-use sqlx::{PgPool, Pool, Postgres};
 use once_cell::sync::OnceCell;
+use sqlx::{PgPool, Pool, Postgres};
 
 pub static PG_POOL: OnceCell<PgPool> = OnceCell::new();
 
 #[inline]
 pub async fn make_pg_pool_connect() {
     match Pool::<Postgres>::connect("postgres://postgres:2006@localhost/carrypigeon").await {
-        Ok(v) => {
-            match PG_POOL.set(v) {
-                Ok(_) => {
-                    tracing::info!("Successfully linked PostgreSQL");
-                }
-                Err(e) => {
-                    tracing::error!("{:?}",e);
-                }
+        Ok(v) => match PG_POOL.set(v) {
+            Ok(_) => {
+                tracing::info!("Successfully linked PostgreSQL");
             }
-        }
+            Err(e) => {
+                tracing::error!("{:?}", e);
+            }
+        },
         Err(e) => {
-            tracing::error!("{}",e);
+            tracing::error!("{}", e);
         }
     }
-     //.connect("postgres://carrypigeon:carrypigeon@localhost/carrypigeon").await
+    //.connect("postgres://carrypigeon:carrypigeon@localhost/carrypigeon").await
 }
 
 pub mod authenticator;
