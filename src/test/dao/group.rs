98754c9fd4498053e32493b8ff20254ea0693ff0
Shirasawa-CN <3489840593@qq.com>
2024-07-19 11:53:07 +0800

    🐛 fix

--- src/test/dao/group.rs
+++ src/test/dao/group.rs
@@ -8,8 +8,7 @@ async fn impl_test_get_member(){
     init_pool().await;
     let a = get_member(0).await;
     let b = get_member(100000000000000000).await;
-    let c: Vec<i64> = vec![0,1];
-    let d: Vec<i64> = Vec::new();
+    let c: Vec<i64> = Vec::new();
     assert_eq!(a,c);
-    assert_eq!(b,d);
+    assert_eq!(b,c);
 }
